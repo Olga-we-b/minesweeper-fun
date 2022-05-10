77373dca60ba185b04cde8eb7b20dae649269e79
Olga Toptunow <o.apiecionek@gmail.com>
2022-05-10 17:16:49 +0200

    count bombs around squares

--- app.js
+++ app.js
@@ -24,6 +24,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
     createBoard();
 
 
+    // add numbers 
+    for (let i =0; i < squares.length; i++){
+        let total = 0;
+        const isLeftEdge = i % width === 0;
+        const isRightEgde = i % width === width -1;
+
+
+        if (squares[i].classList.contains('valid')){
+            if (i > 0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total++;
+            if (i > 9 && !isRightEgde && squares[i+1 - width].classList.contains('bomb')) total++;
+            if (i > 10 && squares[i-width].classList.contains('bomb')) total++;
+            if (i > 11 && !isLeftEdge && squares[i - 1 -width].classList.contains('bomb')) total++;
+            if (i < 98 && !isRightEgde && squares[i + 1].classList.contains('bomb')) total++;
+            if (i < 90 && !isLeftEdge && squares[i-1 + width].classList.contains('bomb')) total++;
+            if (i < 88 && !isRightEgde && squares[i+1 + width].classList.contains('bomb')) total++;
+            if (i < 89 && squares[i + width].classList.contains('bomb')) total++;
+            squares[i].innerText = total;
+
+        }
+    }
+
+
 
 
 
